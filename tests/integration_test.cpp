79e3d1ae3107e307aa7ace78409550a4520e59c5
Peter Lesslie <pclesslie@gmail.com>
2020-04-24 19:17:54 -0500

    WIP

---
+++ tests/integration_test.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <fstream>
+#include <algorithm>
+#include <string>
+#include <optional>
+
+#include <fmt/format.h>
+#include <cxxopts.hpp>
+#include <re2/re2.h>
+
+#include <mafsa++.h>
+
+Mafsa load_dict(std::string filename)
+{
+    if ()
+}
+
+int main(int argc, char **argv)
+{
+    // clang-format off
+    cxxopts::Options options(
+        "integration-test",
+        "replay through official games to test move generation and scoring"
+    );
+    options.add_options()
+        ("d,dict", "dictionary file to use", cxxopts::value<std::string>(), "DICT")
+        ("f,input", "input game file", cxxopts::value<std::vector<std::string>>(), "FILE")
+        ;
+    options.parse_positional({"dict", "input" });
+    // clang-format on
+
+    auto args = options.parse(argc, argv);
+    auto gamefiles = args["input"].as<std::vector<std::string>>();
+    auto dictfile  = args["dict"].as<std::string>();
+
+    const auto dict = load_dict(dictfile);
+
+
+    return 0;
+}
