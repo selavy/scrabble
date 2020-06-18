ada2f56b8709714b854159d90ab5f23470463e5f
Peter Lesslie <pclesslie@gmail.com>
2020-06-17 21:26:50 -0500

    Example unit test that break load_from_position

--- tests/recalc.test.cpp
+++ tests/recalc.test.cpp
@@ -50,7 +50,6 @@ TEST_CASE("Rebuild state after H8 APPLE")
         auto smove = scrabble::Move::from_isc_spec("H8 flip 18");
         auto emove = scrabble::EngineMove::make(&engine1, smove);
         auto* move = &emove.move;
-        cicero_savepos sp;
         cicero_make_move(&engine1, &sp, move);
         copy_position(engine1, board);
         cicero_init(&engine2, cb.make_callbacks());
@@ -69,5 +68,49 @@ TEST_CASE("Rebuild state after H8 APPLE")
             CHECK(isanchorsq(engine1, sq) == isanchorsq(engine2, sq));
         }
 
+    }
+
+    SECTION("After '8H ferrets'")
+    {
+        auto smove = scrabble::Move::from_isc_spec("8H ferrets");
+        auto emove = scrabble::EngineMove::make(&engine1, smove);
+        auto* move = &emove.move;
+        cicero_make_move(&engine1, &sp, move);
+        copy_position(engine1, board);
+        cicero_init(&engine2, cb.make_callbacks());
+        cicero_load_position(&engine2, board);
+
+        for (int sq = 0; sq < 225; ++sq) {
+            INFO("Checking " << hume::Square::make(sq)->name());
+            CHECK(engine1.vals[sq] == engine2.vals[sq]);
+            // using REQUIRE because if any fails, probably they all will
+            CHECK(engine1.hscr[sq] == engine2.hscr[sq]);
+            CHECK(engine1.vscr[sq] == engine2.vscr[sq]);
+            CHECK(engine1.hchk[sq] == engine2.hchk[sq]);
+            CHECK(engine1.vchk[sq] == engine2.vchk[sq]);
+            CHECK(isanchorsq(engine1, sq) == isanchorsq(engine2, sq));
+        }
+    }
+
+    SECTION("After 'L7 lengths'")
+    {
+        auto smove = scrabble::Move::from_isc_spec("L7 lengths");
+        auto emove = scrabble::EngineMove::make(&engine1, smove);
+        auto* move = &emove.move;
+        cicero_make_move(&engine1, &sp, move);
+        copy_position(engine1, board);
+        cicero_init(&engine2, cb.make_callbacks());
+        cicero_load_position(&engine2, board);
+
+        for (int sq = 0; sq < 225; ++sq) {
+            INFO("Checking " << hume::Square::make(sq)->name());
+            CHECK(engine1.vals[sq] == engine2.vals[sq]);
+            // using REQUIRE because if any fails, probably they all will
+            CHECK(engine1.hscr[sq] == engine2.hscr[sq]);
+            CHECK(engine1.vscr[sq] == engine2.vscr[sq]);
+            CHECK(engine1.hchk[sq] == engine2.hchk[sq]);
+            CHECK(engine1.vchk[sq] == engine2.vchk[sq]);
+            CHECK(isanchorsq(engine1, sq) == isanchorsq(engine2, sq));
+        }
     }
 }
