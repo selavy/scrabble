539b5d7943fef1ead930250d2bb1d01c0817907d
Peter Lesslie <pclesslie@gmail.com>
2020-06-20 16:53:14 -0500

    Simplify unit test

--- tests/recalc.test.cpp
+++ tests/recalc.test.cpp
@@ -17,22 +17,35 @@ void copy_position(const cicero& engine, char* board)
     }
 }
 
+void apply_move(cicero* engine, std::string isc_move)
+{
+    auto smove = scrabble::Move::from_isc_spec(isc_move.c_str());
+    auto emove = scrabble::EngineMove::make(engine, smove);
+    auto* move = &emove.move;
+    cicero_savepos sp;
+    cicero_make_move(engine, &sp, move);
+}
+
 TEST_CASE("Rebuild state after H8 APPLE")
 {
     auto cb = make_callbacks({});
     cicero engine1;
     cicero engine2;
     cicero_savepos sp;
-    cicero_init(&engine1, cb.make_callbacks());
-
     char board[225];
+    cicero_init(&engine1, cb.make_callbacks());
+    cicero_init(&engine2, cb.make_callbacks());
 
-    /* "Starting board" */
+    auto load_position = [&cb, &board](const auto* e1, auto* e2)
+    {
+        cicero_init(e2, cb.make_callbacks());
+        copy_position(*e1, board);
+        cicero_load_position(e2, board);
+    };
+
     {
         copy_position(engine1, board);
-        cicero_init(&engine2, cb.make_callbacks());
-        cicero_load_position(&engine2, board);
-
+        load_position(&engine1, &engine2);
         for (int sq = 0; sq < 225; ++sq) {
             INFO("Checking " << hume::Square::make(sq)->name());
             CHECK(engine1.vals[sq] == engine2.vals[sq]);
@@ -44,18 +57,9 @@ TEST_CASE("Rebuild state after H8 APPLE")
         }
     }
 
-    /* "After 'H8 flip 18'" */
     {
-        auto smove = scrabble::Move::from_isc_spec("H8 flip 18");
-        auto emove = scrabble::EngineMove::make(&engine1, smove);
-        auto* move = &emove.move;
-        cicero_make_move(&engine1, &sp, move);
-        copy_position(engine1, board);
-        cicero_init(&engine2, cb.make_callbacks());
-        cicero_load_position(&engine2, board);
-
-        // TODO: actually only care about empty squares matching src and chk
-        // values
+        apply_move(&engine1, "H8 flip");
+        load_position(&engine1, &engine2);
         for (int sq = 0; sq < 225; ++sq) {
             INFO("Checking " << hume::Square::make(sq)->name());
             CHECK(engine1.vals[sq] == engine2.vals[sq]);
@@ -68,20 +72,9 @@ TEST_CASE("Rebuild state after H8 APPLE")
 
     }
 
-    /* "After '8H ferrets'" */
     {
-        auto smove = scrabble::Move::from_isc_spec("8H ferrets");
-        auto emove = scrabble::EngineMove::make(&engine1, smove);
-        auto* move = &emove.move;
-        cicero_make_move(&engine1, &sp, move);
-        copy_position(engine1, board);
-        cicero_init(&engine2, cb.make_callbacks());
-        cicero_load_position(&engine2, board);
-
-        auto I8 = hume::Square::from_isc_name("I8")->value();
-        CHECK(engine1.vals[I8] != 52);
-        CHECK(engine1.vals[I8] == ('e' - 'a'));
-
+        apply_move(&engine1, "8H ferrets");
+        load_position(&engine1, &engine2);
         for (int sq = 0; sq < 225; ++sq) {
             INFO("Checking " << hume::Square::make(sq)->name());
             CHECK(engine1.vals[sq] == engine2.vals[sq]);
@@ -93,16 +86,65 @@ TEST_CASE("Rebuild state after H8 APPLE")
         }
     }
 
-    /* "After 'L7 lengths'" */
     {
-        auto smove = scrabble::Move::from_isc_spec("L7 lengths");
-        auto emove = scrabble::EngineMove::make(&engine1, smove);
-        auto* move = &emove.move;
-        cicero_make_move(&engine1, &sp, move);
-        copy_position(engine1, board);
-        cicero_init(&engine2, cb.make_callbacks());
-        cicero_load_position(&engine2, board);
+        apply_move(&engine1, "L7 lengths");
+        load_position(&engine1, &engine2);
+        for (int sq = 0; sq < 225; ++sq) {
+            INFO("Checking " << hume::Square::make(sq)->name());
+            CHECK(engine1.vals[sq] == engine2.vals[sq]);
+            CHECK(engine1.hscr[sq] == engine2.hscr[sq]);
+            CHECK(engine1.vscr[sq] == engine2.vscr[sq]);
+            CHECK(engine1.hchk[sq] == engine2.hchk[sq]);
+            CHECK(engine1.vchk[sq] == engine2.vchk[sq]);
+            CHECK(isanchorsq(engine1, sq) == isanchorsq(engine2, sq));
+        }
+    }
 
+    {
+        apply_move(&engine1, "7I we");
+        load_position(&engine1, &engine2);
+        for (int sq = 0; sq < 225; ++sq) {
+            INFO("Checking " << hume::Square::make(sq)->name());
+            CHECK(engine1.vals[sq] == engine2.vals[sq]);
+            CHECK(engine1.hscr[sq] == engine2.hscr[sq]);
+            CHECK(engine1.vscr[sq] == engine2.vscr[sq]);
+            CHECK(engine1.hchk[sq] == engine2.hchk[sq]);
+            CHECK(engine1.vchk[sq] == engine2.vchk[sq]);
+            CHECK(isanchorsq(engine1, sq) == isanchorsq(engine2, sq));
+        }
+    }
+
+    {
+        apply_move(&engine1, "13I zoOs");
+        load_position(&engine1, &engine2);
+        for (int sq = 0; sq < 225; ++sq) {
+            INFO("Checking " << hume::Square::make(sq)->name());
+            CHECK(engine1.vals[sq] == engine2.vals[sq]);
+            CHECK(engine1.hscr[sq] == engine2.hscr[sq]);
+            CHECK(engine1.vscr[sq] == engine2.vscr[sq]);
+            CHECK(engine1.hchk[sq] == engine2.hchk[sq]);
+            CHECK(engine1.vchk[sq] == engine2.vchk[sq]);
+            CHECK(isanchorsq(engine1, sq) == isanchorsq(engine2, sq));
+        }
+    }
+
+    {
+        apply_move(&engine1, "12K ThOSe");
+        load_position(&engine1, &engine2);
+        for (int sq = 0; sq < 225; ++sq) {
+            INFO("Checking " << hume::Square::make(sq)->name());
+            CHECK(engine1.vals[sq] == engine2.vals[sq]);
+            CHECK(engine1.hscr[sq] == engine2.hscr[sq]);
+            CHECK(engine1.vscr[sq] == engine2.vscr[sq]);
+            CHECK(engine1.hchk[sq] == engine2.hchk[sq]);
+            CHECK(engine1.vchk[sq] == engine2.vchk[sq]);
+            CHECK(isanchorsq(engine1, sq) == isanchorsq(engine2, sq));
+        }
+    }
+
+    {
+        apply_move(&engine1, "M12 Swim");
+        load_position(&engine1, &engine2);
         for (int sq = 0; sq < 225; ++sq) {
             INFO("Checking " << hume::Square::make(sq)->name());
             CHECK(engine1.vals[sq] == engine2.vals[sq]);
